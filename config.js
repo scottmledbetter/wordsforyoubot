9ff10d06931e5a1cf741ffbdf14e9a0db7fc8f1d
scottmledbetter <scottmledbetter@gmail.com>
2020-04-14 20:31:00 -0700

    Removing stuff

--- config.js
+++
@@ -1,8 +0,0 @@
-var twit = require('twit')
-
-module.exports = {
-  consumer_key:         'Uz71zQh5kXy7oZFwzKAxEQDaK',
-  consumer_secret:      'P5Iw4xtN3Y1yRdClNEf11p2xbukU0C7V6pnJFOT82ov0MuI3Zq',
-  access_token:         '1079821513811841024-VTcKilGDzQ0JNsg1ZeXFf1J1IoXNaF',
-  access_token_secret:  '14VHc2OqMuuyo40caRA6bD082Lz8WCvjQmZSdlINuR7nv'
-}
